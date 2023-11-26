8351a56482cff580b2fff88f522fed67e64bdc07
pearl20-20 <manneshie@yahoo.com>
2023-11-26 13:57:41 +0000

    sorting out aligorithms

--- sort.h
+++ sort.h
@@ -1,13 +1,23 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Comparison direction macros for bitonic sort */
+/*Comparison direction macros for bitonic sort*/
 #define UP 0
 #define DOWN 1
+
+/**
+ * enum bool - Enumeration of boolean values
+*/
+
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,45 +33,25 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 1.
- * @true: Equals 0.
- */
-typedef enum bool
-{
-	true = 0,
-	false
-} bool;
+/*Helper for swapping*/
+void swap_ints(int *a, int *b);
 
+/*Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/*Regular functions prototypes*/
 void bubble_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void swap_values(int *array, int i, int j, int size);
-int lomuto_partition(int *array, int low, int high, int size);
-void recursive_partition(int *array, int low, int high, int size);
-void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-int len_list(listint_t *);
-void swap_nodes(listint_t **, listint_t **, listint_t *);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void swap_node_front(listint_t **, listint_t **, listint_t **);
-void swap_node_behind(listint_t **, listint_t **, listint_t **);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void array_split_recursive(int *, int *, size_t, size_t);
-void merge_subarr(int *, int *, size_t, size_t, size_t);
-void swap_ints(int *, int *);
-void bitonic_merge(int *, size_t, size_t, size_t, char);
 void bitonic_sort(int *array, size_t size);
-void bitonic_seq(int *, long unsigned int, size_t, long unsigned int, char);
-void swap_ints(int *, int *);
-int hoare_partition(int *, size_t, int, int);
-void hoare_sort(int *, size_t, int, int);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
