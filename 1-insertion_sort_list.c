fd50585ee0841c43c5701eee1acda3de264d2e8f
pearl20-20 <manneshie@yahoo.com>
2023-11-26 14:08:01 +0000

    sorting aligorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,56 +1,52 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- *                        order using the Insertion sort algorithm
- *
- * @list: Pointer to a pointer to the head of the list to be sorted
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
+
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers in ascending order
+ * @list: A pointer to the head of the doubly linked list
+ *
+ */
+
+/*
+Time Complexity: O(n^2)
+*/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *back;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		back = current->prev;
-		while (back != NULL && back->n > current->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(&back, &current, list);
-			print_list(*list);
-			back = current->prev;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
-
-/**
- * swap_nodes - swaps two adjacent nodes in a doubly linked list
- *
- * @back: Pointer to a pointer to the back node
- * @current: Pointer to a pointer to the current node
- * @list: Pointer to a pointer to the head of the list
- */
-void swap_nodes(listint_t **back, listint_t **current, listint_t **list)
-{
-	listint_t *tmp = *back;
-
-	/* Adjust the links */
-	(*current)->prev = tmp->prev;
-	if (tmp->prev != NULL)
-		tmp->prev->next = *current;
-	else
-		*list = *current;
-	tmp->next = (*current)->next;
-	if ((*current)->next != NULL)
-		(*current)->next->prev = tmp;
-	(*current)->next = tmp;
-	tmp->prev = *current;
-	*back = *current;
-	*current = tmp;
-}
-
